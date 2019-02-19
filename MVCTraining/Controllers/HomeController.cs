3e645330a8cacc2d87ddaee9a1abdd0777ac78e2
rozycki.mikolaj <rozycki.mikolaj@gmail.com>
2019-02-19 23:22:47 +0100

    Added View Customer

--- MVCTraining/Controllers/HomeController.cs
+++ MVCTraining/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVCTraining.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -25,6 +26,17 @@ namespace MVCTraining.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult ViewCustomer()
+        {
+            Customer customer = new Customer();
+
+            customer.Id = Guid.NewGuid().ToString();
+            customer.Name = "Joe";
+            customer.Telephone = "123123123";
+
+            return View(customer);
         }
     }
 }
